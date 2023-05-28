855988453cb214e63edba574b4cf9b4e11a8e37f
Scott O'Brien <scott@scottyob.com>
2023-05-28 16:29:44 -0700

    Added flight stats

--- app/flying/page.tsx
+++ app/flying/page.tsx
@@ -1,11 +1,13 @@
 import { GetFlights } from "@/lib/flying";
 import FlightTable from "./components/flightTable.react";
 import DescriptionList from "./components/descriptionList.react";
+import FlightStats from "./components/flightStats";
 
 export default async function Flying() {
   const results = await GetFlights();
 
   return <div>
+    <FlightStats flights={results} />
     <div className="hidden sm:block">
       <FlightTable flights={results} />
     </div>
