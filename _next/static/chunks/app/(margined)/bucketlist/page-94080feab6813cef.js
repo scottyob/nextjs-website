652f30fdca6522014e7a4cf5c2b026e01dbca62f
scottyob <scottyob@users.noreply.github.com>
2023-07-27 23:09:45 +0000

    deploy: 8f65c2ef88bf015d93830d4388151a0ce68142b1

--- _next/static/chunks/app/(margined)/bucketlist/page-94080feab6813cef.js
+++ _next/static/chunks/app/(margined)/bucketlist/page-94080feab6813cef.js
@@ -1,13 +1,13 @@
 (self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[857],{
 
-/***/ 4657:
+/***/ 3560:
 /***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 5962, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 8130));
-Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 8469));
-Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 6485));
 Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 8317));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 8469));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 8130));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 6485));
 Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 2346))
 
 /***/ }),
@@ -130,7 +130,7 @@ __webpack_require__.r(__webpack_exports__);
 },
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-/******/ __webpack_require__.O(0, [962,114,253,769,744], function() { return __webpack_exec__(4657); });
+/******/ __webpack_require__.O(0, [962,114,253,769,744], function() { return __webpack_exec__(3560); });
 /******/ var __webpack_exports__ = __webpack_require__.O();
 /******/ _N_E = __webpack_exports__;
 /******/ }
