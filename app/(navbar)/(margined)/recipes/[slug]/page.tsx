817d58e1138e7a6a2495f6dd006c95d1f54b8237
Scott O'Brien <scott@scottyob.com>
2023-12-31 10:54:24 -0800

    [Recipe] Added OpenGraph support

--- app/(navbar)/(margined)/recipes/[slug]/page.tsx
+++ app/(navbar)/(margined)/recipes/[slug]/page.tsx
@@ -5,36 +5,53 @@ const titleFont = DM_Serif_Display({ weight: '400', subsets: ['latin'] });
 const articleFont = DM_Sans({ weight: '400', subsets: ['latin'] });
 import Image from 'next/image';
 import RecipeList from '../components/RecipeList';
+import { Metadata, ResolvingMetadata } from 'next/types';
 
 type RecipeProps = {
   params: { slug: string };
 };
 
+// Pulls out post componet and metadata for a given slug
+async function recipeMetadata(slug: string) {
+  const { frontmatter, default: Component } = await import(
+    `/siteContent/recipes/${slug}/index.mdx`
+  );
+
+  const imgModule = frontmatter.img ? (
+    await import(`/siteContent/recipes/${slug}/${frontmatter.img}`)
+  ).default : undefined;
+
+  return {
+    frontmatter,
+    Component,
+    imgModule
+  }
+}
+
 export default async function Recipe(props: RecipeProps) {
   // Check if this slug matches a category listing
   // and render the recipe list if it's a category
   const recipies = await getAllRecipes();
+  const recipe = recipies.find((r) => r.slug === props.params.slug);
+  if(!recipe) {
+    throw Error("No recipe found");
+  }
+
   const categories = new Set(recipies.flatMap((r) => r.categories));
   if (categories.has(props.params.slug)) {
     return <RecipeList recipies={recipies} filteredTag={props.params.slug} />;
   }
 
   // Load up the MDX post
-  const { frontmatter, default: Component } = await import(
-    `/siteContent/recipes/${props.params.slug}/index.mdx`
-  );
-  const meta = frontmatter;
-  const recipe = recipies.find((r) => r.slug === props.params.slug);
+  const recipeData = await recipeMetadata(props.params.slug);
 
   // Recipe Image
   let img = <></>;
-  if (meta.img != null) {
-    const imgModule = (
-      await import(`/siteContent/recipes/${props.params.slug}/${meta.img}`)
-    ).default;
+  if (recipeData.imgModule) {
+    const imgModule = recipeData.imgModule;
     img = (
       <a href={imgModule.src}>
-        <Image src={imgModule} alt={meta.Title} className="m-auto w-[80%]" />
+        <Image src={imgModule} alt={recipe?.title} className="m-auto w-[80%]" />
       </a>
     );
   }
@@ -50,13 +67,13 @@ export default async function Recipe(props: RecipeProps) {
     <article className="prose">
       <div className={articleFont.className + ' mb-9'}>
         <div className='pb-4 text-center'>
-          <h2 className="pb-1 mb-0">{meta.Title}</h2>
+          <h2 className="pb-1 mb-0">{recipe.title}</h2>
           <div className="text-sm italic">
             {tags}
           </div>
         </div>
         {img}
-        <Component />
+        <recipeData.Component />
       </div>
     </article>
   );
@@ -67,4 +84,28 @@ export async function generateStaticParams() {
   return posts.map((p) => {
     return { slug: p.slug };
   });
+}
+
+export async function generateMetadata(props: RecipeProps, parent: ResolvingMetadata): Promise<Metadata> {
+  const recipies = await getAllRecipes();
+  const recipe = recipies.find((r) => r.slug === props.params.slug);
+  if(!recipe) {
+    throw Error("No recipe found");
+  }
+
+  // Load the metadata and image data from recipe
+  const recipeData = await recipeMetadata(props.params.slug);
+
+  // access and extend parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
+  const title = `Recipe: ${recipe.title}`;
+  return {
+    title, 
+    openGraph: {
+      images: [recipeData.imgModule.src, ...previousImages],
+      title,
+      description: recipe.title
+    }
+  }
 }
